305dec3ef8a8144d5c13b638883308d56cb413e7
asima24 <asima24.barik@gmail.com>
2023-06-07 09:38:17 -0400

    addet cart page and test page

---
+++ src/main/java/Test.java
@@ -0,0 +1,19 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+	
+		//  co.addArguments("--window-size=1920,1080");
+	//  co.addArguments("--no-sandbox");
+	//  co.addArguments("--disable-gpu");
+	//  co.addArguments("--disable-crash-reporter");
+	//  co.addArguments("--disable-extensions");
+	//  co.addArguments("--disable-in-process-stack-traces");
+	//  co.addArguments("--disable-logging");
+	//  co.addArguments("--disable-dev-shm-usage");
+	//  co.addArguments("--log-level=3");
+	//  co.addArguments("--output=/dev/null");// TODO Auto-generated method stub
+
+	}
+
+}
